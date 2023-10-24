f06bd1edee8b615495116c6cdb194e2df78d3807
JVwolfart <jvitor@pinheirasc.com>
2023-10-23 22:15:51 -0300

    Jv 23/10 22:16

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const {execSync} = require('child_process');
 
 
 
-//console.log(obterDiaDaSemana());
-const diaDaSemana = "Segunda-feira";
-//const diaDaSemana = obterDiaDaSemana();
+//const diaDaSemana = "Segunda-feira";
+//const diaDaSemana = "Domingo";
+const diaDaSemana = obterDiaDaSemana();
 
 const clientes = [
     {nome: "Sílvio Santos", email: "silvio@sbt.com", desejaReceberEmail: true},
@@ -23,8 +23,16 @@ const clientes = [
 ]
 
 function enviarEmailParaClientes (clientes) {
+    console.log("Verificando dia da semana...".yellow);
+    execSync("sleep 1");
+    console.log(`Dia da semana: ${diaDaSemana}`.magenta);
+    execSync("sleep 1");
     if (diaDaSemana === "Segunda-feira") {
+        console.log("Gerando vendas...".green);
+        execSync("sleep 1");
         const vendas = geraVendas();
+        console.log("Verificando novidades...".blue);
+        execSync("sleep 1");
         const novidades = geraNovidades();
         for (const cliente of clientes) {
             console.log(`Enviando email para o cliente ${cliente.nome}...`.cyan);
@@ -43,9 +51,15 @@ function enviarEmailParaClientes (clientes) {
             }
             console.log("#####################################################################################".bgCyan + "\n\n");
         }
+        console.log("Todos os emails enviados".green);
+        execSync("sleep 1");
     } else {
         console.log("Hoje não é segunda-feira! O envio de emails para clientes acontece apenas na segunda-feira".yellow);
+        console.log("Encerrando programa...".blue);
+        execSync("sleep 1");
     }
 }
 
 enviarEmailParaClientes(clientes);
+console.log("Encerrando programa...".cyan);
+execSync("sleep 1");
